c53ae79db16cc2279d2700db7a1a4d2aa457e530
obaida28 <obaidaalnaheel283501@gmaul.com>
2023-03-14 13:19:55 +0300

    finished

--- Pages/scripts/__main_js.js
+++ Pages/scripts/__main_js.js
@@ -154,8 +154,8 @@ export function getByName(name){
 }
 export function URL(){
     const local = 'https://localhost:7106'
-    const online = 'http://clinichost-001-site1.ctempurl.com' 
-    const URL_ = local //change here if you want other type!
+    const online = 'http://www.obaida.somee.com/FirstProject' 
+    const URL_ = online //change here if you want other type!
     return URL_;
 }
 export function submit(form_id , functionAPI){
--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 global using Microsoft.EntityFrameworkCore;
 global using System.ComponentModel.DataAnnotations;
 global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.Extensions.FileProviders;
+
 var builder = WebApplication.CreateBuilder(args);
 
 var connectionString = builder.Configuration.GetConnectionString(name:"DefaultConnection");
@@ -31,6 +33,15 @@ app.UseAuthorization();
 
 app.UseCors(c => c.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
 
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(
+           Path.Combine(builder.Environment.ContentRootPath, "Pages")),
+    RequestPath = "/Pages"
+});
+
 app.MapControllers();
 
 app.Run();
+
+/*"Data Source=(localdb)\\ProjectsV13;Initial Catalog=UserOnly;Integrated Security=True;"*/
